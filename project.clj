4ad8d4d63099fdf5f6cc92c75f817ea4b6fb6b20
Nathan Herzing <nherzing@gmail.com>
2013-11-11 16:53:27 -0500

    advanced compilation

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
+
+
 (defproject qrcloj "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
@@ -5,13 +7,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src-clj"]
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2030"]
                  [org.clojure/math.combinatorics "0.0.4"]
                  [org.clojure/math.numeric-tower "0.0.1"]]
- :plugins [[lein-cljsbuild "0.3.2"]]
+ :plugins [[lein-cljsbuild "1.0.0-alpha2"]]
  :cljsbuild {
   :crossovers [qrcloj.encode qrcloj.error-correction qrcloj.utils qrcloj.symbol qrcloj.masking
                qrcloj.binary-field qrcloj.version qrcloj.format clojure.math.combinatorics]
   :builds [{:source-paths ["src-cljs"]
             :compiler {:output-to "out/main.js"
-                       :optimizations :whitespace
+                       :optimizations :advanced
                        :pretty-print true}}]})
